b6436d1aefc873198dcf0be9424e48e2b9165a83
ouch0107 <ouch0107@gmail.com>
2019-08-14 00:18:21 +0800

    modify url

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -4,9 +4,9 @@ from rest_framework import routers
 from blog.api.views import BlogViewSet, BloggerViewSet, CommentViewSet
 
 router = routers.DefaultRouter()
-router.register(r'api/blog', BlogViewSet)
-router.register(r'api/blogger', BloggerViewSet)
-router.register(r'api/comment', CommentViewSet)
+router.register(r'blog', BlogViewSet)
+router.register(r'blogger', BloggerViewSet)
+router.register(r'comment', CommentViewSet)
 
 urlpatterns = [
     path('', include(router.urls))
--- miniblog/urls.py
+++ miniblog/urls.py
@@ -20,7 +20,7 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('admin/', admin.site.urls),
-    path('', include('blog.api.urls')),
+    path('api/', include('blog.api.urls')),
     path('rest-auth/', include('rest_auth.urls')),
     path('rest-auth/registration/', include('rest_auth.registration.urls')),
     re_path('.*', TemplateView.as_view(template_name='index.html'))
